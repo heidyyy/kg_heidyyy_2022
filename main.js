06b69cf6e1cdfeafa1b18ce97375e31c7dbc2fa1
heidyyy <duan23m@mtholyoke.edu>
2021-04-28 00:03:28 -0400

    Add comments on the code and change some variables' name to make the code easy to read

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+//Author Minxi Duan
+// Kargo assessment
+//Convert an array of integers into an array of strings representing the phonetic equivalent of the integer.
 
+// parse command line arguments
 var input = process.argv.slice(2);
-let recipeMap = new Map([
+
+//create a map for integers[0-9] and their phonetic equivalent
+let phoneticMap = new Map([
   [0,'Zero'],
   [1,'One'],
   [2,'Two'],
@@ -12,15 +18,25 @@ let recipeMap = new Map([
   [8,'Eight'],
   [9,'Nine'],
 ]);
+
+// create an empty arry to store the ouptput
 output = [];
-var a = 0;
+
+//loop through each input integer
 for (var num of input){
+    //convert to a string
     sNumber = num.toString();
+    //use n to store converted phonetic equivalent for this integer
     var n = "";
+    //for each integer, find out all the digits and convert to the phonetic equvalent
     for (var i = 0, len = sNumber.length; i < len; i += 1) {
-        n += recipeMap.get(+sNumber.charAt(i));
+        //add each digits of the integer's converted equvalent into n
+        n += phoneticMap.get(+sNumber.charAt(i));
     }
+    //push n into the output array
     output.push(n);
-    var str=output.join(",")
+    //connect the data in output array with ',' and store in a new array named "result"
+    var result = output.join(",")
 }
-console.log(str);
+//print result
+console.log(result);
